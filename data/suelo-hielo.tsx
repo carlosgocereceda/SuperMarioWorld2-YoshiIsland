a36ecbae75859d6da60fe6c3204db3ca4a003702
GDelga <gdelga02@ucm.es>
2019-04-04 23:04:22 +0200

    Yoshi

---
+++ data/suelo-hielo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="suelo-hielo" tilewidth="16" tileheight="16" tilecount="21" columns="7">
+ <image source="../images/Tiles/suelo-hielo.png" width="126" height="58"/>
+</tileset>
